bbeaf488f0b08fdf2a824a70872fcd36a90784f4
Keyle Diaz <140049370+KeyleTherese31@users.noreply.github.com>
2024-04-19 01:43:06 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import Nav from './nav'
+import Nav from './comp/nav'
 import {BrowserRouter} from 'react-router-dom'
 import { Route } from 'router'
+import Rout from './comp/rout'
 
 function App() {
   return (
     <BrowserRouter>
    <Nav/>
+   <Rout />
     </BrowserRouter>
   )
 }
---
+++ src/comp/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className='home'>
+        <div className='top_banner'>
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
